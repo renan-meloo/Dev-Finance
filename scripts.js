fb0d878f32c5a587c28b0f86d7f1de098d681949
renan-meloo <renan.meloo.710@gmail.com>
2021-05-10 19:00:12 -0300

    Automatizando as transações

--- scripts.js
+++ scripts.js
@@ -8,7 +8,6 @@ const Modal = {
     close() {
         document.querySelector('.modal-overlay')
             .classList.remove('active')
-
     }
 }
 
@@ -35,8 +34,9 @@ const transactions = [{
 
 {
     id: 4,
-    description: 'z',
-    amount: -5,
+    description: 'App',
+    amount: 2000000,
+    date: '23/01/2021'
 },
 ]
 
@@ -48,30 +48,47 @@ const Transaction = {
     expenses() {
         // Somas as saídas
 
-
     },
     total() {
         // Entradas - Saídas
-
 
     }
 }
 
 const DOM = {
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transactions.amount)
 
         const html = `
         <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">23/01/2021</td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
             <td>
               <img src="assets/minus.svg" alt="Remover Transação">
             </td>
           </tr>
         `
-
+        return html
     }
-
-
 }
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : " "
+    }
+}
+
+transactions.forEach(function (transaction) {
+    DOM.addTransaction(transaction)
+})
